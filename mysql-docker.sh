0d58be6cf3f1c475fff503f21bd11cd5900ec363
Vipul CEO/CTO <vipul.dadhich@gmail.com>
2019-03-24 23:40:14 -0400

    Updates in the Docker

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -1,9 +1,9 @@
 
-echo 'create database ioplatform' | docker exec -i ioplatform_mysql_1 /usr/bin/mysql -u root --password=root
+#echo 'create database ioplatform' | docker exec -i ioplatform_mysql_1 /usr/bin/mysql -u root --password=password
 # Backup
 #docker exec CONTAINER /usr/bin/mysqldump -u root --password=root DATABASE > backup.sql
 
 # Restore
 
-cat ioplatform.sql | docker exec -i ioplatform_mysql_1 /usr/bin/mysql -u root --password=root ioplatform
+cat ioplatform.sql | docker exec -i ioplatform_mysql_1 /usr/bin/mysql -u root --password=password ioplatform
 
